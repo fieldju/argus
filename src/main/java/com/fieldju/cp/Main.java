5ba111618fabd6a80292d55d3412e59c33bf8bff
Justin Field <fieldju@gmail.com>
2022-03-10 15:01:21 -0800

    chore: initial commit

---
+++ src/main/java/com/fieldju/cp/Main.java
@@ -0,0 +1,11 @@
+package com.fieldju.cp;
+
+public class Main {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
